54488e61de8d7bdf47a79d9345e0e5fdaa85bf9a
Bulb1 <bartoszkawalec90@gmail.com>
2024-12-01 00:19:00 +0100

    - Created template Home page and simple Navbar

---
+++ src/assets/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Navbar() {
+    //TODO: Navigation to Registration/Login
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        Home Stat
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <button>
+                        Add User
+                    </button>
+                </div>
+            </nav>
+        </div>
+    )
+}
